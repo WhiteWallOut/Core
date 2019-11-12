9a2ea201138b26fa73cc289de2491b51c6e2e94c
天祥 王 <530476294@qq.com>
2019-11-12 15:58:04 +0800

    添加项目文件。

---
+++ CoreForStudent/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using CoreForStudent.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreForStudent.Controllers
+{
+    public class HomeController:ControllerBase
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public HomeController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        public   IActionResult Index()
+        {
+            var model =  _studentRepository.GetStudent(1);
+            return Ok();
+        }
+    }
+}
---
+++ CoreForStudent/Models/MockStudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreForStudent.Models
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        private List<Student> _students;
+        public MockStudentRepository()
+        {
+            _students = new List<Student>()
+            {
+                new Student(){Id=1,Name="张三",ClassName="一年级",Email="Tonyzhang@163.com"},
+                new Student(){Id=2,Name="李四",ClassName="二年级",Email="Liaypeng@163.com"},
+                new Student(){Id=3,Name="王五",ClassName="三年级",Email="JackSon@163.com"}
+            };
+          
+        }
+      
+        public  Student GetStudent(int id)
+        {
+           return _students.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
